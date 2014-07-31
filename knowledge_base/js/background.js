d53686e6d204b5feee8d28b6121cdf2e61e33b4b
Rob Lum <lum.robert@gmail.com>
2014-07-31 17:55:04 -0400

    using content scripts to manipulate page

--- knowledge_base/js/background.js
+++ knowledge_base/js/background.js
@@ -1,20 +1,33 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('../main.html', {
-    id: "Offerpop-Knowledge-Base",
-    bounds: {
-      width: 300,
-      height: 500
-    },
-    minWidth: 300,
-    minHeight: 500,
-    frame: 'none'
-  });
+$('.PageContent').on('click',function(){
+  // var currentClass = this.className;
+  // var currentId = this.id;
+  var currentTagName = this.tagName;
+  var children = $(this).children().last();
+
+    // alert(currentClass);
+    // alert(currentId);
+    // alert(currentTagName);
+    console.log(children);
+
 });
+
+// chrome.app.runtime.onLaunched.addListener(function(launchData) {
+//   chrome.app.window.create('../main.html', {
+//     id: "Offerpop-Knowledge-Base",
+//     bounds: {
+//       width: 300,
+//       height: 500
+//     },
+//     minWidth: 300,
+//     minHeight: 500,
+//     frame: 'none'
+//   });
+// });
 
 // chrome.runtime.onInstalled.addListener(function() {
 //   console.log('installed');
 // });
 
-// chrome.runtime.onSuspend.addListener(function() { 
+// chrome.runtime.onSuspend.addListener(function() {
 //   // Do some simple clean-up tasks.
 // });
