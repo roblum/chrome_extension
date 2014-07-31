3f33aab0baf5cfd439884af3baa8814633b89e6c
Robert <lum.robert@gmail.com>
2014-07-31 00:01:48 -0400

    ng-if working double conditional

--- knowledge_base/js/app.js
+++ knowledge_base/js/app.js
@@ -3,7 +3,7 @@ var kbApp = angular.module('kbApp', []);
 kbApp.controller('mainDirectory', ['$scope', function($scope){
 
 	$scope.navigation = [
-		{ name : 'Knowledge Base' , url : 'include.html' }
+		{ name : 'Knowledge Base' , url : 'cssGenerator.html' }
 		,{ name : 'Best Practices' , url : 'include2.html' }
 	];
 
@@ -13,11 +13,34 @@ kbApp.controller('mainDirectory', ['$scope', function($scope){
 kbApp.controller('cssCompiler', ['$scope',function($scope){
 
 	$scope.cssList = [
-		{ name : 'Background' , content : 'background' }
-		, { name : 'Text Color' , content : 'color' }
-		, { name : 'Border Radius' , content : 'border-radius' }
-		, { name : 'Font' , content : 'font-family' }
-		, { name : 'Font Size' , content : 'font-size' }
+		{name : 'Background' 
+			,content : 'background:' 
+			,px : false
+		}
+		,{name : 'Text Color' 
+			,content : 'color:' 
+			,px : false
+			}
+		,{name : 'Border Radius' 
+			,content : 'border-radius:' 
+			,px : true
+		}
+		,{name : 'Font'
+			,content : 'font-family:' 
+			,px : false
+		}
+		,{name : 'Font Size' 
+			,content : 'font-size:' 
+			,px : true
+		}
+		,{name : 'Width'
+			,content : 'width:'
+			,px : true
+		}
+		,{name : 'Height'
+			,content : 'height:'
+			,px : true
+		}
 	];
 
 }]);
