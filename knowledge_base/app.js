332c9800f05221547ff564badc640547709ee0c4
Robert <lum.robert@gmail.com>
2014-07-30 23:02:29 -0400

    organized files

--- knowledge_base/app.js
+++
@@ -1,8 +0,0 @@
-var kbApp = angular.module('kb-app', []);
-
-kbApp.controller('mainDirectory', function($scope){
-	$scope.items = {
-		'knowledge base' : 'knowledge base value'
-		,'best practices' : 'best practices value'
-	}
-});
