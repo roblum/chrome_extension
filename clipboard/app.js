ba75681f0d59284b4563a28a0cfefcc93c27a101
Rob Lum <lum.robert@gmail.com>
2014-09-15 16:22:41 -0400

    setting up folder for clipboard extension

---
+++ clipboard/app.js
@@ -0,0 +1,6 @@
+var clipApp = angular.module('clipApp', []);
+
+clipApp.controller('mainDirectory', ['$scope', function($scope){
+
+
+}]);
---
+++ clipboard/main.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('copy-button');
+
+$('body').on('click', '#copy-button', function(){
+     var area = document.querySelector('#copy-box');
+     area.focus();
+     document.execCommand('SelectAll');
+    document.execCommand("Copy", false, null);
+});
