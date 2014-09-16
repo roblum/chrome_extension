7e430a503ef99b91ea6a4c3ae61cc60f7ff4d73a
Robert <lum.robert@gmail.com>
2014-09-16 00:41:47 -0400

    trying to add events to copy buttons WIP

--- clipboard/main.js
+++ clipboard/main.js
@@ -1,17 +1,35 @@
-// var button = document.getElementById('copy-button');
-
-// $('body').on('click', '#copy-button', function(){
-//      var area = document.querySelector('#copy-box');
-//      area.focus();
-//      document.execCommand('SelectAll');
-//     document.execCommand("Copy", false, null);
-// });
-
 window.onload = function(){
-$('.snippet-container').mouseenter(function(){
-	console.log('fired');
-	$(this).find('.copy-button').fadeIn();
-}).mouseleave(function(){
-	$(this).find('.copy-button').fadeOut();
-});
+
+	copyHover();
+	attachCopy();
+
 }
+
+	function copyHover(){
+		$('.snippet-container').mouseenter(function(){
+			console.log('fired');
+			$(this).find('.copy-button').fadeIn();
+		}).mouseleave(function(){
+			$(this).find('.copy-button').fadeOut();
+		});
+	}
+
+	function attachCopy(){
+		var button = document.querySelectorAll('.copy-button');
+		console.log(button);
+
+		for (var i=0; i<button.length;i++){
+			console.log(button[i].id);
+			$('#copy-' + button[i].id).click(function(){
+				console.log(this);
+				console.log('hello');
+			});
+		}
+
+		// $('body').on('click', '#copy-button', function(){
+		//      var area = document.querySelector('#copy-box');
+		//      area.focus();
+		//      document.execCommand('SelectAll');
+		//     document.execCommand("Copy", false, null);
+		// });
+	}
