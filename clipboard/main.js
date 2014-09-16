ab83954da3120859ba3af9ae02831075d853699c
Robert <lum.robert@gmail.com>
2014-09-16 00:59:16 -0400

    copy working on each respective input

--- clipboard/main.js
+++ clipboard/main.js
@@ -6,30 +6,26 @@ window.onload = function(){
 }
 
 	function copyHover(){
+
 		$('.snippet-container').mouseenter(function(){
 			console.log('fired');
 			$(this).find('.copy-button').fadeIn();
 		}).mouseleave(function(){
 			$(this).find('.copy-button').fadeOut();
 		});
+		
 	}
 
 	function attachCopy(){
-		var button = document.querySelectorAll('.copy-button');
-		console.log(button);
 
-		for (var i=0; i<button.length;i++){
-			console.log(button[i].id);
-			$('#copy-' + button[i].id).click(function(){
-				console.log(this);
-				console.log('hello');
-			});
-		}
+		$('body').on('click', '.copy-button', function(){
+			var current = this.id.match(/\d+/g)[0];
+			console.log(current);
+		    
+		    var area = document.querySelector('#snippet-container-' + current + ' input');
+		    area.focus();
+		    document.execCommand('SelectAll');
+		    document.execCommand("Copy", false, null);
+		});
 
-		// $('body').on('click', '#copy-button', function(){
-		//      var area = document.querySelector('#copy-box');
-		//      area.focus();
-		//      document.execCommand('SelectAll');
-		//     document.execCommand("Copy", false, null);
-		// });
 	}
