03b5fb5be7b02e58993fc7ccdd772ee4c21921fe
Robert <lum.robert@gmail.com>
2014-09-16 00:09:02 -0400

    added copy button on input hover

--- clipboard/main.js
+++ clipboard/main.js
@@ -1,8 +1,17 @@
-var button = document.getElementById('copy-button');
+// var button = document.getElementById('copy-button');
 
-$('body').on('click', '#copy-button', function(){
-     var area = document.querySelector('#copy-box');
-     area.focus();
-     document.execCommand('SelectAll');
-    document.execCommand("Copy", false, null);
+// $('body').on('click', '#copy-button', function(){
+//      var area = document.querySelector('#copy-box');
+//      area.focus();
+//      document.execCommand('SelectAll');
+//     document.execCommand("Copy", false, null);
+// });
+
+window.onload = function(){
+$('.snippet-container').mouseenter(function(){
+	console.log('fired');
+	$(this).find('.copy-button').fadeIn();
+}).mouseleave(function(){
+	$(this).find('.copy-button').fadeOut();
 });
+}
