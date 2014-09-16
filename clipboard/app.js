218610c0c4580332e738063e158dabd8d08abcf8
Robert <lum.robert@gmail.com>
2014-09-15 23:34:54 -0400

    ng-repeat on inputs with incrementing ID

--- clipboard/app.js
+++ clipboard/app.js
@@ -2,5 +2,12 @@ var clipApp = angular.module('clipApp', []);
 
 clipApp.controller('mainDirectory', ['$scope', function($scope){
 
+	$scope.snippets = [
+		{
+			"one" : "one"
+		},{
+			"eno" : "eno"
+		}
+	];
 
 }]);
