6c6e6cc1f3d9955a936f52660e777d6a23facbdd
Robert <lum.robert@gmail.com>
2014-09-18 23:01:35 -0400

    basic jquery login/registration working; proof of concept

--- clipboard/main.js
+++ clipboard/main.js
@@ -22,30 +22,38 @@ window.onload = function(){
 // we would probably save a profile when we register new users on our site
 // we could also read the profile to see if it's null
 // here we will just simulate this with an isNewUser boolean
-var isNewUser = true;
+// var isNewUser = true;
 
 var myRef = new Firebase("https://clipboard-list.firebaseio.com");
 var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
+  console.log('user');
+  console.log(user);
   if (error) {
     // an error occurred while attempting login
+    console.log('error');
     console.log(error);
   } else if (user) {
     // user authenticated with Firebase
     console.log("User ID: " + user.uid + ", Provider: " + user.provider);
-    if( isNewUser ) {
-      // save new user's profile into Firebase so we can
-      // list users, use them in security rules, and show profiles
-      myRef.child('users').child(user.uid).set({
-        displayName: user.displayName,
-        provider: user.provider,
-        provider_id: user.id
-      });
-    }
+    $('#login-container').hide();
+    $('#copy-list-container').show();
+    // if( isNewUser ) {
+    //   // save new user's profile into Firebase so we can
+    //   // list users, use them in security rules, and show profiles
+    //   myRef.child('users').child(user.uid).set({
+    //     displayName: user.displayName,
+    //     provider: user.provider,
+    //     provider_id: user.id
+    //   });
+    // }
   } else {
+    $('#login-button-container').show();
+    console.log('user is logged out');
     // user is logged out
   }
 });
 
+function authentication(){
 var authRef = new Firebase("https://clipboard-list.firebaseio.com/.info/authenticated");
      authRef.on("value", function(snap) {
   if (snap.val() === true) {
@@ -54,12 +62,13 @@ var authRef = new Firebase("https://clipboard-list.firebaseio.com/.info/authenti
     alert("not authenticated");
   }
 });
+   }
 
 $('body').on('click', '#login-submit',function(){
   var username = $('#username').val()
     ,password = $('#password').val();
 
-  register(username, password);
+  // register(username, password);
   login(username, password);
 
 });
@@ -87,4 +96,6 @@ function login(username, password){
     password: password,
     rememberMe: true
     });
+
+    // authentication();
 }
