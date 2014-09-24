0813f1381e6b28157ced7e4924ae5aca6c7ab836
Rob Lum <lum.robert@gmail.com>
2014-09-24 13:46:37 -0400

    WIP; firebase login works with static info. need to connect view to factory?

--- bug_tracker/js/app.js
+++ bug_tracker/js/app.js
@@ -2,19 +2,6 @@ var bugApp = angular.module('bugApp', ['ngAnimate', 'firebase']);
 
 bugApp.controller('mainDirectory', function($scope, $firebase){
 
-    var ref = new Firebase("https://bug-tracker.firebaseio.com/snippets");
-
-    // create an AngularFire reference to the data
-    var sync = $firebase(ref);
-
-    // download the data into a local object
-    $scope.snippets = sync.$asArray();
-
-    $scope.addSnippet = function(text){
-    	console.log('snippet ran');
-    	$scope.snippets.$add({text: text});
-    }
-
 	$scope.button = function($index){
 		var current = $index
 	    	,area = document.querySelector('#snippet-container-' + current + ' input');
@@ -36,6 +23,71 @@ bugApp.controller('mainDirectory', function($scope, $firebase){
 
 });
 
+
+
+
+bugApp.controller('MainCtrl', function ($scope, $firebaseSimpleLogin, $firebase, dataService) {
+     var ref = new Firebase("https://clipboard-list.firebaseio.com/snippets");
+     // create an AngularFire reference to the data
+     var sync = $firebase(ref);
+
+     // $scope.login = false;
+
+     // download the data into a local object
+     $scope.snippets = sync.$asArray();
+
+     $scope.addSnippet = function(text){
+          console.log('snippet ran');
+          $scope.snippets.$add({text: text});
+     }
+
+
+     var loginConfirmation = function() {
+          // This service's function returns a promise, but we'll deal with that shortly
+               dataService.getLogin()
+                    // then() called when son gets back
+                    .then(function(data) {
+                         // promise fulfilled
+                         // if (data.forecast==='good') {
+                         //      // prepareFishingTrip();
+                         // } else {
+                         //      // prepareSundayRoastDinner();
+                         // }
+                    }, function(error) {
+                         // promise rejected, could log the error with: console.log('error', error);
+                         // prepareSundayRoastDinner();
+                    });
+     };
+
+});
+
+
+bugApp.factory('dataService', function ($http, $q) {
+    return {
+        getLogin: function() {
+            // the $http API is based on the deferred/promise APIs exposed by the $q service
+            // so it returns a promise for us by default
+               var firebaseRef = new Firebase('https://bug-tracker.firebaseio.com/');
+               $scope.auth = $firebaseSimpleLogin(firebaseRef);
+
+               $scope.dummySignin = function() {
+                    console.log('signing in...');
+
+                    $scope.auth.$login('password', {
+                         email: 'asdf@asdf.asdf',
+                         password: 'asdf'
+                    }).then(function(user) {
+                         console.log('user: ', user);
+                         $scope.login = true;
+                    }, function(error) {
+                         console.log('error: ', error);
+                         $scope.login = false;
+                    });
+               };
+
+        }
+    };
+});
 
 // bugApp.controller("MyAuthCtrl", ["$scope", "$firebaseSimpleLogin",
 
