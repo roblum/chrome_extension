daf22a4f306b17154069b907236cbf2a348895ce
Rob Lum <lum.robert@gmail.com>
2014-09-26 09:17:16 -0400

    focus on login for now. angularfire login is working; need to start asset pulling from firebase

--- bug_tracker/js/app.js
+++ bug_tracker/js/app.js
@@ -1,7 +1,56 @@
 var bugApp = angular.module('bugApp', ['ngAnimate', 'firebase']);
 
-bugApp.controller('mainDirectory', function($scope, $firebase){
 
+bugApp.controller('mainDirectory', ["$scope", "$firebaseSimpleLogin", function($scope, $firebaseSimpleLogin){
+
+     var dataRef = new Firebase("https://bug-tracker.firebaseio.com/");
+     $scope.loginObj = $firebaseSimpleLogin(dataRef);
+     console.log('initiated controller');
+
+
+     $scope.loginObj.$getCurrentUser()
+          .then(function(data){
+               // Returns true if user is logged in
+               console.log("data");
+               console.log(data);
+               $scope.authFlag = data !== null;
+          }, function(){
+               console.log('no data');
+               $scope.authFlag = false;
+          });
+
+     $scope.checkCredentials = function(user){
+          // $scope.logins = angular.copy($scope.user);
+
+          console.log('running credentials');
+          $scope.loginObj.$login("password", {
+               email: user.name,
+               password: user.pass
+          }).then(function(user) {
+               console.log("Logged in as: ", user.uid);
+               console.log("Logged in as: " + user.id + ' ' + user);
+          }, function(error) {
+               console.error("Login failed: ", error);
+          });
+
+     }
+
+     // $scope.logout = function(){
+     //      $scope.auth.$logout();
+     //      $scope.authFlag = false;
+     // };
+
+
+          // $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+          //   console.log("User " + user.id + " successfully logged in!");
+          // });
+
+
+
+
+     /***********************************/
+     /***********************************/
+     /***********************************/
 	$scope.button = function($index){
 		var current = $index
 	    	,area = document.querySelector('#snippet-container-' + current + ' input');
@@ -21,93 +70,4 @@ bugApp.controller('mainDirectory', function($scope, $firebase){
 		this.hover = false;
 	}
 
-});
-
-
-
-
-bugApp.controller('MainCtrl', function ($scope, $firebaseSimpleLogin, $firebase, dataService) {
-     var ref = new Firebase("https://clipboard-list.firebaseio.com/snippets");
-     // create an AngularFire reference to the data
-     var sync = $firebase(ref);
-
-     // $scope.login = false;
-
-     // download the data into a local object
-     $scope.snippets = sync.$asArray();
-
-     $scope.addSnippet = function(text){
-          console.log('snippet ran');
-          $scope.snippets.$add({text: text});
-     }
-
-
-     var loginConfirmation = function() {
-          // This service's function returns a promise, but we'll deal with that shortly
-               dataService.getLogin()
-                    // then() called when son gets back
-                    .then(function(data) {
-                         // promise fulfilled
-                         // if (data.forecast==='good') {
-                         //      // prepareFishingTrip();
-                         // } else {
-                         //      // prepareSundayRoastDinner();
-                         // }
-                    }, function(error) {
-                         // promise rejected, could log the error with: console.log('error', error);
-                         // prepareSundayRoastDinner();
-                    });
-     };
-
-});
-
-
-bugApp.factory('dataService', function ($http, $q) {
-    return {
-        getLogin: function() {
-            // the $http API is based on the deferred/promise APIs exposed by the $q service
-            // so it returns a promise for us by default
-               var firebaseRef = new Firebase('https://bug-tracker.firebaseio.com/');
-               $scope.auth = $firebaseSimpleLogin(firebaseRef);
-
-               $scope.dummySignin = function() {
-                    console.log('signing in...');
-
-                    $scope.auth.$login('password', {
-                         email: 'asdf@asdf.asdf',
-                         password: 'asdf'
-                    }).then(function(user) {
-                         console.log('user: ', user);
-                         $scope.login = true;
-                    }, function(error) {
-                         console.log('error: ', error);
-                         $scope.login = false;
-                    });
-               };
-
-        }
-    };
-});
-
-// bugApp.controller("MyAuthCtrl", ["$scope", "$firebaseSimpleLogin",
-
-// 	function($scope, $firebaseSimpleLogin) {
-// 	    var dataRef = new Firebase("https://clipboard-list.firebaseio.com");
-// 	    $scope.loginObj = $firebaseSimpleLogin(dataRef);
-// 	    console.log('simple login angular fire ran');
-// 	    console.log($scope.loginObj);
-
-// 	      	$scope.loginObj.$login("password", {
-// 			   email: "asdf@asdf.asdf",
-// 			   password: "asdf"
-// 			}).then(function(user) {
-// 			   console.log("Logged in as: ", user.uid);
-// 			}, function(error) {
-// 			   console.error("Login failed: ", error);
-// 			});
-
-// 			$rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
-// 		  		console.log("User " + user.id + " successfully logged in!");
-// 			});
-// 	}
-// ]);
+}]);
