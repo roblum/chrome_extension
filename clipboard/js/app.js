59a3b75dd4c73015f92fdab8bd98d58ff5899a33
Robert <lum.robert@gmail.com>
2014-09-30 22:45:12 -0400

    reverting back login work; testing in bugApp instead; building out copy & paste functionality (working); need to add remove & edit

--- clipboard/js/app.js
+++ clipboard/js/app.js
@@ -2,18 +2,19 @@ var clipApp = angular.module('clipApp', ['ngAnimate', 'firebase']);
 
 clipApp.controller('mainDirectory', function($scope, $firebase){
 
-    var ref = new Firebase("https://clipboard-list.firebaseio.com/snippets");
+      	var ref = new Firebase("https://clipboard-list.firebaseio.com/snippets");
 
-    // create an AngularFire reference to the data
-    var sync = $firebase(ref);
+		    // create an AngularFire reference to the data
+	    	var sync = $firebase(ref);
 
-    // download the data into a local object
-    $scope.snippets = sync.$asArray();
+		    // download the data into a local object
+		    $scope.snippets = sync.$asArray();
 
-    $scope.addSnippet = function(text){
-    	console.log('snippet ran');
-    	$scope.snippets.$add({text: text});
-    }
+		    $scope.addSnippet = function(text){
+		    	console.log('snippet ran');
+		    	$scope.snippets.$add({text: text});
+		    }
+
 
 	$scope.button = function($index){
 		var current = $index
@@ -36,26 +37,3 @@ clipApp.controller('mainDirectory', function($scope, $firebase){
 
 });
 
-
-// clipApp.controller("MyAuthCtrl", ["$scope", "$firebaseSimpleLogin",
-	
-// 	function($scope, $firebaseSimpleLogin) {
-// 	    var dataRef = new Firebase("https://clipboard-list.firebaseio.com");
-// 	    $scope.loginObj = $firebaseSimpleLogin(dataRef);
-// 	    console.log('simple login angular fire ran');
-// 	    console.log($scope.loginObj);
-
-// 	      	$scope.loginObj.$login("password", {
-// 			   email: "asdf@asdf.asdf",
-// 			   password: "asdf"
-// 			}).then(function(user) {
-// 			   console.log("Logged in as: ", user.uid);
-// 			}, function(error) {
-// 			   console.error("Login failed: ", error);
-// 			});
-
-// 			$rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
-// 		  		console.log("User " + user.id + " successfully logged in!");
-// 			});
-// 	}
-// ]);
