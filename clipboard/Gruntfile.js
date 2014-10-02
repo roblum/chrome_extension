4b5fe306cca1fec4b249bd4aa27b54dc25809018
Rob Lum <lum.robert@gmail.com>
2014-10-02 14:24:25 -0400

    adding package.json/grunt for stylus & uglify

---
+++ clipboard/Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            dist: {
+                files: {
+                    'js/app.min.js': 'js/app.js'
+                }
+                ,options: {
+                    banner: '/* Clipboard - <%= grunt.template.today() %> */'
+                }
+            },
+        }
+        ,watch: {
+            js : {
+              files: ['js/app.js']
+              ,tasks: ['uglify:dist']
+            }
+            ,css : {
+              files: ['styles.styl']
+              ,tasks: ['stylus']
+            }
+        },stylus: {
+            compile: {
+                options: {
+                    compress: true
+                    ,lineos: false
+                }
+                ,files: {
+                  'styles.css': 'styles.styl'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['uglify']);
+
+
+};
