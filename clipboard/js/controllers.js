b94f71a396f79e354588f70af94bdffe85bbc8b4
Rob Lum <lum.robert@gmail.com>
2014-10-07 11:16:24 -0400

    moved all old functionality to new controller; needs refactoring

--- clipboard/js/controllers.js
+++ clipboard/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('clipApp.controllers', [])
   .controller('HomeController', [function() {
 
   }])
-  .controller('InfoController', ['$scope', 'infoService', 'textMessageService', 'authService', function($scope, infoService, textMessageService, authService) {
+  .controller('InfoController', ['$scope', 'infoService', 'authService', function($scope, infoService, authService) {
 
     // Bind user's parties to $scope.parties.
     authService.getCurrentUser().then(function(user) {
@@ -20,6 +20,49 @@ angular.module('clipApp.controllers', [])
     $scope.getNewData = function(){
       infoService.directoryPull($scope.currentDirectory);
     }
+
+    $scope.addSnippet = function(text){
+            console.log('snippet ran');
+            $scope.snippets.$add({text: text});
+            document.querySelector('form input').value = '';
+        }
+
+    $scope.edit = function($index){
+      var current = document.querySelector('#snippet-container-' + $index + ' input');
+      current.removeAttribute('disabled');
+    }
+
+    $scope.button = function($index){
+      var current = $index
+          ,area = document.querySelector('#snippet-container-' + current + ' input');
+
+            area.removeAttribute('disabled');
+            area.focus();
+            document.execCommand('SelectAll');
+            document.execCommand("Copy", false, null);
+            area.setAttribute('disabled', 'disabled');
+
+                 var currDir = $scope.currentDir.$id;
+
+                 if (area.value.indexOf('http') === 0){ //!(currDir.indexOf('Links') === -1) &&
+                      chrome.tabs.create({url: area.value});
+                 }
+      }
+
+      $scope.disable = function($index){
+          document.querySelector('#snippet-container-' + $index + ' input')
+              .setAttribute('disabled','disabled');
+      }
+
+      $scope.hover = false;
+
+      $scope.show = function(){
+        this.hover = true;
+      }
+
+      $scope.hide = function(){
+        this.hover = false;
+      }
 
   }])
   .controller('AuthController', ['$scope', 'authService', function($scope, authService) {
@@ -38,68 +81,3 @@ angular.module('clipApp.controllers', [])
     };
 
   }]);
-
-
-.controller('mainDirectory', function($scope, $firebase){
-     var directory = 'https://clipboard-list.firebaseio.com/';
-
-     var rootRef = new Firebase(directory)
-          ,rootSync = $firebase(rootRef);
-
-     $scope.rootItems = rootSync.$asArray();
-
-     $scope.$watch('currentDir', function(value){
-          if (value){
-               var ref = new Firebase(directory + value.$id)
-                    ,sync = $firebase(ref); // create an AngularFire reference to the data
-
-               // download the data into a local object
-               $scope.snippets = sync.$asArray();
-          }
-     });
-
-        $scope.addSnippet = function(text){
-            console.log('snippet ran');
-            $scope.snippets.$add({text: text});
-            document.querySelector('form input').value = '';
-        }
-
-  $scope.edit = function($index){
-    var current = document.querySelector('#snippet-container-' + $index + ' input');
-    current.removeAttribute('disabled');
-  }
-
-  $scope.button = function($index){
-    var current = $index
-        ,area = document.querySelector('#snippet-container-' + current + ' input');
-
-          area.removeAttribute('disabled');
-          area.focus();
-          document.execCommand('SelectAll');
-          document.execCommand("Copy", false, null);
-          area.setAttribute('disabled', 'disabled');
-
-               var currDir = $scope.currentDir.$id;
-
-               if (area.value.indexOf('http') === 0){ //!(currDir.indexOf('Links') === -1) &&
-                    chrome.tabs.create({url: area.value});
-               }
-  }
-
-  $scope.disable = function($index){
-      document.querySelector('#snippet-container-' + $index + ' input')
-          .setAttribute('disabled','disabled');
-  }
-
-  $scope.hover = false;
-
-  $scope.show = function(){
-    this.hover = true;
-  }
-
-  $scope.hide = function(){
-    this.hover = false;
-  }
-
-});
-
