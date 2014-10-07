0bbe87a6b7a5bac2d6b2c05b19a5059e77747004
Rob Lum <lum.robert@gmail.com>
2014-10-07 10:35:30 -0400

    implementing routes, 3 states / /login /info

--- clipboard/js/app.js
+++ clipboard/js/app.js
@@ -1,65 +1,24 @@
-var clipApp = angular.module('clipApp', ['ngAnimate', 'firebase']);
+'use strict';
 
-clipApp.controller('mainDirectory', function($scope, $firebase){
-     var directory = 'https://clipboard-list.firebaseio.com/';
-
-     var rootRef = new Firebase(directory)
-          ,rootSync = $firebase(rootRef);
-
-     $scope.rootItems = rootSync.$asArray();
-
-     $scope.$watch('currentDir', function(value){
-          if (value){
-               var ref = new Firebase(directory + value.$id)
-                    ,sync = $firebase(ref); // create an AngularFire reference to the data
-
-               // download the data into a local object
-               $scope.snippets = sync.$asArray();
-          }
-     });
-
-		    $scope.addSnippet = function(text){
-			    	console.log('snippet ran');
-			    	$scope.snippets.$add({text: text});
-			    	document.querySelector('form input').value = '';
-		    }
-
-	$scope.edit = function($index){
-		var current = document.querySelector('#snippet-container-' + $index + ' input');
-		current.removeAttribute('disabled');
-	}
-
-	$scope.button = function($index){
-		var current = $index
-	    	,area = document.querySelector('#snippet-container-' + current + ' input');
-
-	    		area.removeAttribute('disabled');
-		    	area.focus();
-		    	document.execCommand('SelectAll');
-		    	document.execCommand("Copy", false, null);
-		    	area.setAttribute('disabled', 'disabled');
-
-               var currDir = $scope.currentDir.$id;
-
-               if (area.value.indexOf('http') === 0){ //!(currDir.indexOf('Links') === -1) &&
-                    chrome.tabs.create({url: area.value});
-               }
-	}
-
-  $scope.disable = function($index){
-      document.querySelector('#snippet-container-' + $index + ' input')
-          .setAttribute('disabled','disabled');
-  }
-
-	$scope.hover = false;
-
-	$scope.show = function(){
-		this.hover = true;
-	}
-
-	$scope.hide = function(){
-		this.hover = false;
-	}
-
-});
-
+angular.module('clipApp', [
+  'ngRoute',
+  'ngAnimate',
+  'myApp.services',
+  'myApp.controllers',
+  'firebase'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'HomeController'
+  });
+  $routeProvider.when('/login', {
+    templateUrl: 'partials/login.html',
+    controller: 'AuthController'
+  });
+  $routeProvider.when('/info', {
+    templateUrl: 'partials/info.html',
+    controller: 'InfoController'
+  });
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);
